4e06f4640937ec01c1f2416dae7983d36b211721
Donathan <mockus49@gmail.com>
2023-10-10 16:15:14 +0200

    Project: split folders into different cmake files

---
+++ src/IR/Ir.cpp
@@ -0,0 +1,9 @@
+/*
+ * Copyright (c) 2023 Donatas Mockus.
+ */
+
+//
+// Created by aelliixx on 2023-10-10.
+//
+
+#include "Ir.h"
---
+++ src/IR/Ir.h
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2023 Donatas Mockus.
+ */
+
+//
+// Created by aelliixx on 2023-10-10.
+//
+
+#pragma once
+
+namespace alx::ir {
+
+class Ir
+{
+
+};
+}
